9e7a2dd767b336e42044287018ee17f73c10b2ab
Andrei <kryvetskistud@gmail.com>
2021-04-13 00:53:54 +0300

    feat: add implement 11 task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,31 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const result = [];
+  let arr = str.split('');
+
+  for (let i = 1; i < arr.length; i++) {
+    if (i === arr.length - 1 && arr[i] === arr[i - 1]) {
+      result.push(arr.length);
+      result.push(arr[i]);
+    }
+
+    if (arr[i] !== arr[i - 1]) {
+      if (i !== 1) {
+        result.push(i);
+      }
+
+      result.push(arr[i - 1]);
+      arr = arr.slice(i, arr.length);
+      i = 0;
+
+      if (arr.length === 1) {
+        result.push(arr[0]);
+      }
+    }
+  }
+  return result.map((el) => String(el)).join('');
 }
 
 module.exports = encodeLine;
